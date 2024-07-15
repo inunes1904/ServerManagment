019e013fd3261111a7eb181fbbb3e8504891efac
Ivo Nunes <inunes1904@gmail.com>
2024-07-15 22:41:31 +0100

    Application starter to add some servers

---
+++ src/main/java/com/ivo_nunes/servermanagment/ServermanagmentApplication.java
@@ -0,0 +1,26 @@
+package com.ivo_nunes.servermanagment;
+
+import com.ivo_nunes.servermanagment.enumeration.Status;
+import com.ivo_nunes.servermanagment.model.Server;
+import com.ivo_nunes.servermanagment.repo.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ServermanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServermanagmentApplication.class, args);
+	}
+
+  CommandLineRunner run(ServerRepository serverRepository){
+    return args -> {
+      serverRepository.save(new Server(null,"192.168.1.160", "Ubuntu Linux", "16GB", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+      serverRepository.save(new Server(null,"192.168.1.161", "Fedora Linux", "16GB", "Personal SERVER", "http://localhost:8080/server/image/server1.png", Status.SERVER_DOWN));
+      serverRepository.save(new Server(null,"192.168.1.162", "Ubuntu Linux", "16GB", "Professional PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+      serverRepository.save(new Server(null,"192.168.1.163", "Test Linux", "16GB", "Professional PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_DOWN));
+    };
+  }
+}
