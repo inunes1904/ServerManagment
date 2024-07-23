ea30589cfa7670433a9e77ad6e686a2a5096dcc3
Ivo Nunes <inunes1904@gmail.com>
2024-07-23 23:26:38 +0100

    app component increments

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ServerService} from "./service/server.service";
+import {map, Observable} from "rxjs";
+import {AppState} from "./interface/app-state";
+import {CustomResponse} from "./interface/custom-response";
+import {DataState} from "./enum/data-state.enum";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent implements OnInit{
+
+  appState$: Observable<AppState<CustomResponse>>;
+
+  constructor(private serverService: ServerService) {
+  }
+
+  ngOnInit(){
+    this.appState$ = this.serverService.servers$
+      .pipe(map(response => {
+        return {dataState: DataState.LOADED_STATE, appData: response}
+      }))
+  }
+
 }
